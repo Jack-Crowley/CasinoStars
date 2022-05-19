ba2e6f511b0c4d430b6fc75956e7126f91bd70bf
Jacob Berger <tkdpenguin123@gmail.com>
2022-05-19 16:04:01 -0400

    Added a card and deck class

---
+++ cards.js
@@ -0,0 +1,42 @@
+class Deck {
+    constructor() {
+        let suits = ['clubs', 'spades', 'hearts', 'diamonds']
+        let values = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+        this.deck = [];
+        for (let i = 0; i < suits.length; i++) {
+            for (let n = 0; n < values.length; n++) {
+                this.deck.push(new Card(suits[i], values[n]));
+            }
+        }
+        this.str = function() {
+            'TO DO LATER';
+        }
+    }
+
+    draw() {
+        let card = this.deck[0];
+        this.deck.splice(0, 1);
+        return card;
+    }
+
+    shuffle() {
+        // Fisher-Yates algorithm
+        for (let i = this.deck.length - 1; i > 0; i--) {
+            // randomly choose a remaining element
+            let j = Math.floor(Math.random() * (i + 1))
+            let temp = this.deck[i]
+
+            // swap the positions of the chosen element with the current element
+            this.deck[i] = this.deck[j]
+            this.deck[j] = temp;
+        }
+    }
+}
+
+class Card {
+    constructor(suit, value) {
+        this.suit = suit;
+        this.value = value;
+        this.str = value + " of " + suit;
+    }
+}
