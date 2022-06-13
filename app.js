eaae7fb4ad46aa45be24f630f329239a5e5d5dd7
Jack-Crowley <jcrowley.jjc@gmail.com>
2022-06-13 07:49:24 -0400

    PORT

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require( "express" );
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
 const logger = require("morgan");
 const db = require('./db/db_connection');
 
